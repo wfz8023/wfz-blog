ac1aa908b73e0863adcbc79e05f6379cb9dda786
wfz8023 <wfz_8023@163.com>
2019-11-10 15:53:33 +0800

    使用webpack

---
+++ src/App/index.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import './App.css';
+
+
+class Index extends React.Component {
+  public render() {
+    return (
+      <div className="App">
+3333333
+      </div>
+    );
+  }
+}
+
+export default Index;
